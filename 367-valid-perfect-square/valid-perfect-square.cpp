1c7bd79b600b897c4eec8f151b521d930459e612
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2025-11-17 00:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.60%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPerfectSquare(long num) {
+        if(num<2){
+            return true;
+
+        }
+        int left=2;
+        int right=num/2;
+        while(left<=right){
+            long mid= (left+right)/2;
+            if(mid*mid==num){
+                return true;
+            }else if(mid*mid<num){
+                left= mid +1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
