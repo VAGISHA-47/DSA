d16580a29870a6d294c78f5d5bc9ea5fd3914e08
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2026-03-21 00:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (23.28%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    stringstream ss(s);
+    vector<string> words;
+    string word;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    reverse(words.begin(), words.end());
+
+    string result = "";
+    for (string w : words) {
+        result += w + " ";
+    }
+
+    result.pop_back(); 
+    return result;
+    }
+};
+
+    
