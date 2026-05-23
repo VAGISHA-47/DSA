bbe41f089c84774f4a8a1ce9f39581e8a30c79fe
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2026-05-23 11:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (79.95%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,11 +4,11 @@ public:
         int currentSum = nums[0];
         int maxSum = nums[0];
 
-        for(int i = 1; i < nums.size(); i++) {
+        for(int i=1 ; i<nums.size();i++){
             currentSum = max(nums[i], currentSum + nums[i]);
             maxSum = max(maxSum, currentSum);
-        }
 
+        }
         return maxSum;
     }
 };
