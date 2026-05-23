7bcba34cf141696640144baeaff5ed4b7577fd6a
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2026-05-23 11:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (15.02%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,10 +4,11 @@ public:
         int left =0;
         int right = s.size() -1;
         while(left<right){
-            char tmp = s[left];
-            s[left++]=s[right];
+            char tmp =s[left];
+            s[left++] = s[right];
             s[right--] = tmp;
         }
+      
         
     }
 };
