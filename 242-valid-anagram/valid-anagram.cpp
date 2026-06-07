0c19015fb0c4625397e05bd65c84d7627f294416
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2026-06-07 23:25:29 +0530

    Time: 4 ms (34.13%) | Memory: 9.7 MB (75.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,23 +3,12 @@ public:
     bool isAnagram(string str1, string str2) {
         if(str1.length()!=str2.length()){
             return false;
+        }
+        sort(str1.begin(),str1.end());
+        sort(str2.begin(),str2.end());
 
-        }
-        int count[26]={0};
-        for(int i=0;i<str1.length();i++){
-            int idx=str1[i]-'a';
-            count[idx]++;
-        }
-        for(int i=0;i<str2.length();i++){
-            int idx=str2[i]-'a';
-            if(count[idx]==0){
-                return false;
-            }
-            count[idx]--;
-        }
-        return true;
-                
-
+        return str1==str2;
+        
     }
     int main(){
         string str1;
